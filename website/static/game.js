b14b1e2267f3e1793601fbd971f4186fe7fc9a5a
PaulVicino <47858768+PaulVicino@users.noreply.github.com>
2021-08-30 10:10:22 -0400

    Add files via upload

--- website/static/game.js
+++ website/static/game.js
@@ -4,23 +4,31 @@ let gameScene = new Phaser.Scene('Game');
 // Load assets
 gameScene.preload = function(){
     // load background and other assets
-    this.load.image('background', '/assets/table_layout.png');
+    this.load.image('background', '/static/assets/table_layout.png');
 };
 
 // called a single time after preload ends
 gameScene.create = function(){
     let bg = this.add.sprite(0, 0, 'background');
 
-    bg.setOrigin(0,0);
+    // bg.setOrigin(0,0);
+
+    bg.setPosition(1200/2, 990/2);
+
+    let gameW = this.sys.game.config.width;
+    let gameH = this.sys.game.config.height;
 };
 
 // set configuration of the game
 let config = {
     type: Phaser.AUTO, // uses WebGL if available, otherwise uses canvas
     width: 1200,
-    height: 1000,
+    height: 990,
     scene: gameScene,
 };
 
 // create a new game, and pass the configuration
 let game = new Phaser.Game(config);
+
+
+// https://youtu.be/hI_LS8bdkM4?t=2321
